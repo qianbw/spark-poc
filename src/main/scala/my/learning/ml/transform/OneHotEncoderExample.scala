1dbc78966e5200791f037f01461f96d9e8841cc3
qianbw <biweiqian@webank.com>
2017-07-13 09:57:49 +0800

    哑编码

--- src/main/scala/my/learning/ml/transform/OneHotEncoderExample.scala
+++ src/main/scala/my/learning/ml/transform/OneHotEncoderExample.scala
@@ -1,11 +1,17 @@
 package my.learning.ml.transform
 
-import org.apache.spark.ml.feature.OneHotEncoder
-import org.apache.spark.ml.feature.StringIndexer
-import org.apache.spark.sql.SparkSession
 import scala.collection.Seq
 import scala.reflect.api.materializeTypeTag
 
+import org.apache.spark.ml.feature.OneHotEncoder
+import org.apache.spark.ml.feature.StringIndexer
+import org.apache.spark.sql.SparkSession
+
+/**
+ * 是一种状态编码。因为有些特征是用文字分类表达的，或者说将这些类转化为数字，但是数字与数字之间是没有大小关系的，纯粹的分类标记，
+ * 这时候就需要用哑编码对其进行编码。比如0用0001,1用0010，2用0100以此类推。有点类似文本矩阵，最终会构成一个稀疏矩阵。
+ * 参考：http://blog.csdn.net/big_talent/article/details/53887238
+ */
 object OneHotEncoderExample {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession
